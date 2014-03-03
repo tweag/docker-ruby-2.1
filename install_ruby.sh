9251c8446eec6d1508d3abb1cd64c24d60a4fd3d
Justin Campbell <justin@justincampbell.me>
2014-03-03 17:34:37 -0500

    Move install code to external script

---
+++ install_ruby.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+export RUBY_INSTALL_VERSION=0.4.0
+export RUBY_VERSION=2.1.1
+
+cd /tmp
+
+yum list installed | cut -f 1 -d " " | uniq | sort > /tmp/pre
+yum install git -y
+
+wget -O ruby-install-$RUBY_INSTALL_VERSION.tar.gz \
+  https://github.com/postmodern/ruby-install/archive/v$RUBY_INSTALL_VERSION.tar.gz
+tar -xzf ruby-install-$RUBY_INSTALL_VERSION.tar.gz
+cd ruby-install-$RUBY_INSTALL_VERSION
+make install
+ruby-install -i /usr/local ruby $RUBY_VERSION
+
+make uninstall
+yum list installed | cut -f 1 -d " " | uniq | sort > /tmp/post
+diff /tmp/pre /tmp/post | grep "^>" | cut -f 2 -d ' ' | xargs echo yum erase -y
+yum clean all
+rm -rf /usr/local/src/ruby*
+rm -rf /tmp/*
